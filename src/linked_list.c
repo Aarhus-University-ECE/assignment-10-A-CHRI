18e154d5611194ca188e6bc520258cf1e1615e57
Alexander Anker Christensen <alexander.Anker.chr@gmail.com>
2022-11-14 23:20:40 +0100

    Added answers

--- src/linked_list.c
+++ src/linked_list.c
@@ -5,7 +5,8 @@
 #include "linked_list.h"
 
 /* functions to create lists */
-node *make_node(int v, node *q) {
+node *make_node(int v, node *q)
+{
   node *p = malloc(sizeof(node));
   p->value = v;
   p->next = q;
@@ -13,9 +14,11 @@ node *make_node(int v, node *q) {
 }
 
 /* free all nodes in the list p */
-void free_list(node *p) {
+void free_list(node *p)
+{
   node *q;
-  while (p != NULL) {
+  while (p != NULL)
+  {
     q = p->next;
     free(p);
     p = q;
@@ -23,23 +26,58 @@ void free_list(node *p) {
 }
 
 /* print list to console */
-void print_list(node *p) {
-  // Add your code for exercise 1
-  // There is NO testcode for this
+void print_list(node *p)
+{
+  /* Base case: Last element in the list (next = NULL)*/
+  if (p->next == NULL)
+  {
+    printf("%d", p->value);
+  }
+  /* Recursive step: */
+  else
+  {
+    print_list(p->next);
+    printf("%d", p->value);
+  }
 }
 
-int sum_squares(node *p) {
-  // Add your code for excercise 2
-  // You can find the tests in tests.cpp
-  return -1;
+int sum_squares(node *p)
+{
+  /* Base case: Empty node */
+  if (p == NULL)
+  {
+    return 0;
+  }
+  /* Base case: Last element in the list */
+  if (p->next == NULL)
+  {
+    return (p->value) * (p->value);
+  }
+  /* Recursive step: */
+  else
+  {
+    return (p->value) * (p->value) + sum_squares(p->next);
+  }
 }
 
 typedef int (*fn_int_to_int)(int);
 
-node *map(node *p, fn_int_to_int f) { 
-  // Add your code for excercise 3
-  
-  return NULL; 
+node *map(node *p, fn_int_to_int f)
+{
+  /* Pre: Non-empty list */
+  assert(p != NULL);
+
+  /* Base case: Last node */
+  if(p->next == NULL) {
+    node * q = make_node( f(p->value), NULL);
+    return q;
+  }
+  /* Recursive step: */
+  else {
+    node * q = make_node( f(p->value), NULL);
+    q->next = map(p->next, f);
+    return q;
+  }
 }
 
 int square(int x) { return x * x; }
